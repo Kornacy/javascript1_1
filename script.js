72fc53cf229e04a2332eabd4f98e1c0a19154ab7
Kamil Buszta <kamilbuszta.3@gmail.com>
2025-10-14 09:14:31 +0000

    C 1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 (function () {
-  //TODO
+  
+  document.getElementById("ex1_button").addEventListener("click", function () {
+    var tab=[];
+    for(let i = 0; i < 10;i++){
+    tab.push(i);
+    }
+    document.getElementById("ex1_content").innerHTML = tab;
+    })
 })();
