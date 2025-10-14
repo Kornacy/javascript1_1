90311f31dcf37f209aeeb30c571fa0459e9667b8
Kamil Buszta <kamilbuszta.3@gmail.com>
2025-10-14 09:42:26 +0000

    C 1_1.2

--- script.js
+++ script.js
@@ -7,4 +7,22 @@
     }
     document.getElementById("ex1_content").innerHTML = tab;
     })
+    document.getElementById("ex2_text").addEventListener("input", function ()
+    {
+      var phone = document.getElementById("ex2_text").value; 
+      var regex = /[a-zA-Z]/;
+      var regex2 = /[\W]/;;
+      if (phone.length != 9) {
+        document.getElementById("ex2_content").innerHTML = "Długość numeru musi być równa 9";
+      }
+      else if (regex.test(phone)) {
+          document.getElementById("ex2_content").innerHTML = "Numer nie może zawierać liter";
+      }
+      else if (regex2.test(phone)) {
+        document.getElementById("ex2_content").innerHTML = "Numer nie może zawierać znaków specjalnych";
+      }
+      else {
+        document.getElementById("ex2_content").innerHTML = "Numer telefonu jest poprawny";
+      }
+    })
 })();
