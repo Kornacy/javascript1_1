3e84b2157e1200576f2abe5c0ff39b0f492eba83
Kamil <kamilbuszta.3@gmail.com>
2025-10-18 19:57:51 +0200

    C 1_1.2 - Poprawione

--- script.js
+++ script.js
@@ -12,18 +12,18 @@
     {
       var phone = document.getElementById("ex2_text").value; 
       var regex = /[a-zA-Z]/;
-      var regex2 = /[\W]/;;
-      if (phone.length != 9) {
-        document.getElementById("ex2_content").innerHTML = "Długość numeru musi być równa 9";
-      }
-      else if (regex.test(phone)) {
-          document.getElementById("ex2_content").innerHTML = "Numer nie może zawierać liter";
+      var regex2 = /[^0-9]/;
+      if (regex.test(phone)) {
+          document.getElementById("ex2_content").textContent = "Numer nie może zawierać liter";
       }
       else if (regex2.test(phone)) {
-        document.getElementById("ex2_content").innerHTML = "Numer nie może zawierać znaków specjalnych";
+        document.getElementById("ex2_content").textContent = "Numer nie może zawierać znaków specjalnych";
+      }
+      else if (phone.length != 9) {
+        document.getElementById("ex2_content").textContent = "Długość numeru musi być równa 9";
       }
       else {
-        document.getElementById("ex2_content").innerHTML = "Numer telefonu jest poprawny";
+        document.getElementById("ex2_content").textContent = "Numer telefonu jest poprawny";
       }
     })
 })();
